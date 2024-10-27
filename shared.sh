55f5170b4a841db336cc96f125df7173579c90c7
AntonioMR <antonio1010.mr@gmail.com>
2024-10-27 20:00:08 +0100

    Create buildroot configuration for beaglebone black hardware Resolves https://github.com/AntonioMR/AESD_CANbus_Analyzer/issues/1

--- shared.sh
+++ shared.sh
@@ -3,9 +3,13 @@
 
 # The defconfig from the buildroot directory we use for qemu builds
 QEMU_DEFCONFIG=configs/qemu_aarch64_virt_defconfig
+BEAGLEBONE_BLACK_DEFCONFIG=configs/beaglebone_defconfig
+
 # The place we store customizations to the qemu configuration
 MODIFIED_QEMU_DEFCONFIG=base_external/configs/aesd_qemu_defconfig
+MODIFIED_BEAGLEBONE_BLACK_DEFCONFIG=base_external/configs/aesd_beaglebone_black_defconfig
+
 # The defconfig from the buildroot directory we use for the project
-AESD_DEFAULT_DEFCONFIG=${QEMU_DEFCONFIG}
-AESD_MODIFIED_DEFCONFIG=${MODIFIED_QEMU_DEFCONFIG}
+AESD_DEFAULT_DEFCONFIG=${BEAGLEBONE_BLACK_DEFCONFIG}
+AESD_MODIFIED_DEFCONFIG=${MODIFIED_BEAGLEBONE_BLACK_DEFCONFIG}
 AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT=../${AESD_MODIFIED_DEFCONFIG}
